5aa937508ae11cf59b7dde5da68040d7f469dd23
P3Vu <38510303+P3Vu@users.noreply.github.com>
2018-05-07 07:17:21 +0200

    Update tb2_modul.vhd
    testbench pod 7

--- tb2_modul.vhd
+++ tb2_modul.vhd
@@ -2,10 +2,10 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   12:33:49 04/17/2018
+-- Create Date:   03:35:12 05/06/2018
 -- Design Name:   
--- Module Name:   C:/Documents and Settings/Pawel/Desktop/iup/Zadanie 6/OdNowa6/Zad6_3/Zad6/tb_modul.vhd
--- Project Name:  Zad6
+-- Module Name:   C:/Documents and Settings/Pawel/Desktop/iup/zad7/zad7_2/tb.vhd
+-- Project Name:  zad7_2
 -- Target Device:  
 -- Tool versions:  
 -- Description:   
@@ -30,17 +30,16 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.ALL;
  
-ENTITY tb2_modul IS
-END tb2_modul;
+ENTITY tb3 IS
+END tb3;
  
-ARCHITECTURE behavior OF tb2_modul IS 
+ARCHITECTURE behavior OF tb3 IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT Modul
     PORT(
-         btn_i : IN  std_logic_vector(3 downto 0);
-         sw_i : IN  std_logic_vector(7 downto 0);
+         start_stop_button_i : IN  std_logic;
          rst_i : IN  std_logic;
          clk_i : IN  std_logic;
          led7_seg_o : OUT  std_logic_vector(7 downto 0);
@@ -50,8 +49,7 @@ ARCHITECTURE behavior OF tb2_modul IS
     
 
    --Inputs
-   signal btn_i : std_logic_vector(3 downto 0) := (others => '0');
-   signal sw_i : std_logic_vector(7 downto 0) := (others => '0');
+   signal start_stop_button_i : std_logic := '0';
    signal rst_i : std_logic := '0';
    signal clk_i : std_logic := '0';
 
@@ -66,8 +64,7 @@ BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: Modul PORT MAP (
-          btn_i => btn_i,
-          sw_i => sw_i,
+          start_stop_button_i => start_stop_button_i,
           rst_i => rst_i,
           clk_i => clk_i,
           led7_seg_o => led7_seg_o,
@@ -88,39 +85,28 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100ms.
-	  		wait for 100us;
-			sw_i <= "00001111" ;		-- F
-	   wait for 100us;
-			btn_i <= "0001" ;
-	   wait for 100us;
-			btn_i <= "0000" ;
-
-			wait for 100us;
-			sw_i <= "00100010" ;		-- 2.
-	   wait for 100us;
-			btn_i <= "0010" ;
-	   wait for 100us;
-			btn_i <= "0000" ;
-
-			wait for 100us;
-			sw_i <= "00000001" ;		-- 1
-	   wait for 100us;
-			btn_i <= "0100" ;
-	   wait for 100us;
-			btn_i <= "0000" ;
-
-	  wait for 100us;
-			sw_i <= "10001010" ;		-- A.
-	   wait for 100us;
-			btn_i <= "1000" ;
-	   wait for 100us;
-			btn_i <= "0000" ;
-			
-
+      wait for 50us;	
+			start_stop_button_i <= '1' ;
+		wait for 500us ;
+			start_stop_button_i <= '0' ;
+		wait for 2000us ;
+			rst_i <= '1' ;
+		wait for 500us ;
+			rst_i <= '0' ;
+		wait for 500us;
+			start_stop_button_i <= '1' ;
+		wait for 500us ;
+			start_stop_button_i <= '0' ;
+		wait for 500us;
+					start_stop_button_i <= '1' ;
+		wait for 500us ;
+			start_stop_button_i <= '0' ;
+					wait for 500us ;
+					start_stop_button_i <= '1' ;
+		wait for 500us ;
+			start_stop_button_i <= '0' ;
+		
       wait for clk_i_period*10;
-
-      -- insert stimulus here 
-
       wait;
    end process;
 
